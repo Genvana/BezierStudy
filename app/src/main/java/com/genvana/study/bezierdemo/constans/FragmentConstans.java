4deda004b17bcf3025c6358d7f05745564bc4cbf
Genvana <476934048@qq.com>
2016-11-15 17:56:04 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/genvana/study/bezierdemo/constans/FragmentConstans.java
@@ -0,0 +1,14 @@
+package com.genvana.study.bezierdemo.constans;
+
+/**
+ * Created by Genvana on 2016/11/15.
+ */
+
+public class FragmentConstans {
+    public static final int QuadBezier = 0x01;
+    public static final int CubicBezier = 0x10;
+    public static final int PathMorphBezier = 0x11;
+    public static final int WaveBezier = 0x100;
+    public static final int PathBezier = 0x101;
+
+}
